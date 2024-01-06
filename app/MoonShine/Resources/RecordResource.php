850440b75516c8b9d0f69921d8be185b112927b8
Islomzoda <islomzodo17@gmail.com>
2024-01-06 20:30:26 +0300

    export fix

--- app/MoonShine/Resources/RecordResource.php
+++ app/MoonShine/Resources/RecordResource.php
@@ -10,6 +10,7 @@ use App\Models\Record;
 use MoonShine\Fields\Number;
 use MoonShine\Fields\Phone;
 use MoonShine\Fields\Text;
+use MoonShine\Handlers\ExportHandler;
 use MoonShine\Resources\ModelResource;
 use MoonShine\Decorations\Block;
 use MoonShine\Fields\ID;
@@ -25,15 +26,22 @@ class RecordResource extends ModelResource
         return [
             Block::make([
                 ID::make()->sortable(),
-                Text::make('ФИО', 'fio')->readonly(),
-                Phone::make('Номер Телефона', 'phone')->readonly(),
-                Number::make('Доход', 'maosh')->readonly(),
-                Text::make('Работает в', 'working_in')->readonly(),
-                Text::make('Телеграм ИД', 'telegram_id')->readonly()
+                Text::make('ФИО', 'fio')->showOnExport()->readonly(),
+                Phone::make('Номер Телефона', 'phone')->showOnExport()->readonly(),
+                Number::make('Доход', 'maosh')->showOnExport()->readonly(),
+                Text::make('Работает в', 'working_in')->showOnExport()->readonly(),
+                Text::make('Телеграм ИД', 'telegram_id')->showOnExport()->readonly()
             ]),
         ];
     }
-
+    public function export(): ?ExportHandler
+    {
+        return ExportHandler::make('Export')
+            // Выбор диска
+            ->disk('public')
+            // Выбор директории сохранения файла экспорта
+            ->dir('/exports');
+   }
     public function rules(Model $item): array
     {
         return [];
